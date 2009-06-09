519bda9cb4aae0be4a316e2841818408fc2e9099
Gillis Danielsen <gillis@localhost.localdomain>
2009-06-09 18:33:17 +0200

    added /analysis/setFileName macro, still not perfect

---
+++ src/HadrontherapyAnalysisFileMessenger.cc
@@ -0,0 +1,71 @@
+//
+// ********************************************************************
+// * License and Disclaimer                                           *
+// *                                                                  *
+// * The  Geant4 software  is  copyright of the Copyright Holders  of *
+// * the Geant4 Collaboration.  It is provided  under  the terms  and *
+// * conditions of the Geant4 Software License,  included in the file *
+// * LICENSE and available at  http://cern.ch/geant4/license .  These *
+// * include a list of copyright holders.                             *
+// *                                                                  *
+// * Neither the authors of this software system, nor their employing *
+// * institutes,nor the agencies providing financial support for this *
+// * work  make  any representation or  warranty, express or implied, *
+// * regarding  this  software system or assume any liability for its *
+// * use.  Please see the license in the file  LICENSE  and URL above *
+// * for the full disclaimer and the limitation of liability.         *
+// *                                                                  *
+// * This  code  implementation is the result of  the  scientific and *
+// * technical work of the GEANT4 collaboration.                      *
+// * By using,  copying,  modifying or  distributing the software (or *
+// * any work based  on the software)  you  agree  to acknowledge its *
+// * use  in  resulting  scientific  publications,  and indicate your *
+// * acceptance of all terms of the Geant4 Software license.          *
+// ********************************************************************
+//
+// See more at: http://workgroup.lngs.infn.it/geant4lns/
+//
+// ----------------------------------------------------------------------------
+//                 GEANT 4 - Hadrontherapy example
+// ----------------------------------------------------------------------------
+// Code developed by:
+//
+// G.A.P. Cirrone(a)*
+// 
+// (a) Laboratori Nazionali del Sud 
+//     of the INFN, Catania, Italy
+// 
+// * cirrone@lns.infn.it
+// ----------------------------------------------------------------------------
+#include "HadrontherapyAnalysisFileMessenger.hh"
+#include "HadrontherapyAnalysisManager.hh"
+#include "G4UIcmdWithAString.hh"
+#include "G4UIdirectory.hh"
+//
+/////////////////////////////////////////////////////////////////////////////
+HadrontherapyAnalysisFileMessenger::HadrontherapyAnalysisFileMessenger(HadrontherapyAnalysisManager* amgr)
+:AnalysisManager(amgr)
+{ 
+  FileNameCmd = new G4UIcmdWithAString("/analysis/setAnalysisFile",this);
+  FileNameCmd->SetGuidance("Set the .root filename for the root-output");
+  FileNameCmd->SetDefaultValue("default.root");
+  FileNameCmd->SetParameterName("choice",true); //doc did not say what second boolean really does
+  FileNameCmd->AvailableForStates(G4State_Idle,G4State_PreInit);
+}
+
+/////////////////////////////////////////////////////////////////////////////
+HadrontherapyAnalysisFileMessenger::~HadrontherapyAnalysisFileMessenger()
+{
+  delete FileNameCmd;
+}
+
+/////////////////////////////////////////////////////////////////////////////
+void HadrontherapyAnalysisFileMessenger::SetNewValue(G4UIcommand* command, G4String newValue)
+{ 
+  if (command == FileNameCmd)
+    {
+	AnalysisManager->SetAnalysisFileName(newValue);
+	AnalysisManager->book(); //books the histograms etc. again in new file, doesen't remove old one
+    }
+}
+
