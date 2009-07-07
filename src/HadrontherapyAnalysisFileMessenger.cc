cronops/hadrontherapy

commit 238c44397e7f788051dc51b7400d7a9479709d08

now also flushes the matrix inbetween runns correctly

src/HadrontherapyAnalysisFileMessenger.cc
@@ -68,7 +68,7 @@ void HadrontherapyAnalysisFileMessenger::SetNewValue(G4UIcommand* command, G4Str
     {
 	AnalysisManager->SetAnalysisFileName(newValue);
 	AnalysisManager->flush(); //< fills matrix, writes it into file and books a new file and histograms etc.
-    AnalysisManager->book();
+    //AnalysisManager->book();
 	}
 }
 

doc/thesis/macro/braggPeaks.mac
@@ -61,12 +61,12 @@
 
 #########
 #400MeV case
-#/analysis/setAnalysisFile DoseDistribution200MeV.root
-#/gun/particle He3
-#/beam/energy/meanEnergy 200 MeV
-#/beam/energy/sigmaEnergy 400 keV
-#/beam/position/Xposition -1 mm
+/analysis/setAnalysisFile DoseDistribution200MeV.root
+/gun/particle He3
+/beam/energy/meanEnergy 200 MeV
+/beam/energy/sigmaEnergy 400 keV
+/beam/position/Xposition -1 mm
 #/run/beamOn 5000
 #testrun
-/run/beamOn 50
+/run/beamOn 100
 

include/HadrontherapyMatrix.hh
@@ -57,6 +57,8 @@ public:
  
   static HadrontherapyMatrix* getInstance();
  
+ void flush();
+ 
   void Initialize(); 
   // All the elements of the matrix are initialised to zero
  

src/HadrontherapyAnalysisManager.cc
@@ -498,6 +498,7 @@ void HadrontherapyAnalysisManager::genericIonInformation(G4int a,
 void HadrontherapyAnalysisManager::flush()
 {
   HadrontherapyMatrix* matrix = HadrontherapyMatrix::getInstance();
+
   matrix->TotalEnergyDeposit();
 #ifdef G4ANALYSIS_USE
   theTree -> commit();
@@ -507,8 +508,10 @@ void HadrontherapyAnalysisManager::flush()
   theROOTNtuple->Write();
   theROOTIonTuple->Write();
   theTFile->Write();
+  theTFile->Clear();
   theTFile->Close();
 #endif
+  matrix->flush();
   //this->book();
 }
 /////////////////////////////////////////////////////////////////////////////

src/HadrontherapyMatrix.cc
@@ -73,6 +73,14 @@ HadrontherapyMatrix::~HadrontherapyMatrix()
 {
   delete[] matrix;
 }
+
+void HadrontherapyMatrix::flush(){
+	if(matrix)
+		for(int i=0;i<numberVoxelX*numberVoxelY*numberVoxelZ;i++){
+		matrix[i] = 0;
+		}
+	}
+
 void HadrontherapyMatrix::Initialize()
 { 
   // Initialise the elemnts of the matrix to zero

commit 26aa4d3d93248f9ba2c755ea03b0262dde82896e

Bugfix: AnalysisManager->book() call was commented
This fixes error:
Error in <TROOT::WriteTObject>: The current directory (root) is not associated
with a file. The object (theROOTNtuple) has not been written.
Error in <TROOT::WriteTObject>: The current directory (root) is not associated
with a file. The object (theROOTIonTuple) has not been written.
Error in <TROOT::WriteTObject>: The current directory (root) is not associated
with a file. The object (ThinTargetBeam) has not been written.
Warning in <TFile::Write>: file DoseDistribution.root not opened in write mode
Graphics systems deleted.

The bug was introduced in commit 238c44397e7f788051dc51b7400d7a9479709d08

src/HadrontherapyAnalysisFileMessenger.cc
@@ -68,7 +68,7 @@ void HadrontherapyAnalysisFileMessenger::SetNewValue(G4UIcommand* command, G4Str
     {
 	AnalysisManager->SetAnalysisFileName(newValue);
 	AnalysisManager->flush(); //< fills matrix, writes it into file and books a new file and histograms etc.
-    //AnalysisManager->book();
+	AnalysisManager->book();
 	}
 }
 