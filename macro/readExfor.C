8d1342be38c93a16b712aefbf433c17a623823ae
Gillis Danielsen <gillis@localhost.localdomain>
2009-07-08 16:19:35 +0200

    now plots the data

--- macro/readExfor.C
+++ macro/readExfor.C
@@ -18,11 +18,11 @@ Float_t f1,f2,f3, f4,f5,f6;
    Int_t nlines = 0;
    TFile *f = new TFile("basic.root","RECREATE");
    TH1F *h1 = new TH1F("h1","x distribution",100,-4,4);
-   TNtuple *ntuple = new TNtuple("ntuple","data from ascii file","x:y:z");
+   TNtuple *ntuple = new TNtuple("ntuple","data from ascii file","Energy:He:B:H:Li:Be");
 	  
    Char_t DATAFLAG[4];
    Int_t NDATA;
-   Char_t n1[2], n2[2], n3[2], n4[2], n5[2], n6[2],;
+   Char_t n1[6], n2[2], n3[2], n4[2], n5[2], n6[2],;
    in >> DATAFLAG >> NDATA ; // Read EXFOR line: 'DATA 6'
    in >> n1 >> n2 >> n3 >> n4 >> n5 >> n6; // Read  column titles: 'Energy He B [...]'
 
@@ -31,11 +31,17 @@ Float_t f1,f2,f3, f4,f5,f6;
       in >> f1 >> f2 >> f3 >>f4 >> f5 >> f6;
       if (!in.good()) break;
       if (nlines < 500 ) printf("%i  %0.2f %0.2f %0.2f %0.2f %0.2f \n",f1,f2,f3,f4,f5,f6);
-      h1->Fill(f1);
-      ntuple->Fill(f1,f2,f3);
+      //h1->Fill(f1);
+      ntuple->Fill(f1,f2,f3,f4,f5,f6);
       nlines++;
    }
-   printf(" found %d points\n",nlines);
+   ntuple->SetMarkerStyle(5);
+   ntuple->Draw("He:Energy","","l");
+   ntuple->Draw("B:Energy","","l,Same");
+   ntuple->Draw("H:Energy","","l,Same");
+   ntuple->Draw("Li:Energy","","l,Same");
+   ntuple->Draw("Be:Energy","","l,Same");
+   //printf(" found %d points\n",nlines);
 
    in.close();
 
