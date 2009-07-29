37863869a65c555c58c03d60d0edd88228cd1431
Gillis Danielsen <gillis@localhost.localdomain>
2009-07-29 15:52:45 +0200

    notthing special small improvements while trying out detectorSD - which has some bug

--- src/HadrontherapyDetectorSD.cc
+++ src/HadrontherapyDetectorSD.cc
@@ -72,6 +72,7 @@ void HadrontherapyDetectorSD::Initialize(G4HCofThisEvent*)
 
 G4bool HadrontherapyDetectorSD::ProcessHits(G4Step* aStep, G4TouchableHistory* ROhist)
 {
+		//The code doesen't seem to get here. FIXME
   if(!ROhist)
     return false;
  
